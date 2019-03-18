0f50d3f4706246afe18469d35601e0c55921b98e
Will Usher <willusher.life@gmail.com>
2019-03-18 14:28:37 -0600

    update

--- webgl-neuron/js/swc.js
+++ webgl-neuron/js/swc.js
@@ -4,6 +4,7 @@ var SWCTree = function(swcFile, name) {
 	this.branches = [];
 	this.indices = []
 	this.points = []
+	this.numSoma = 0;
 
 	this.vao = null;
 	this.vbo = null;
@@ -24,13 +25,18 @@ var SWCTree = function(swcFile, name) {
 		if (id == 1) {
 			branch = { "start": 0 };
 			this.indices.push(0);
+			this.numSoma = 1;
 		} else if (parentID != id - 1) {
 			branch["count"] = this.indices.length - branch["start"];
 			this.branches.push(branch);
 
 			branch = { "start": this.indices.length };
 			// IDs in the file start at 1
-			this.indices.push(parentID - 1);
+			if (parentID != -1) {
+				this.indices.push(parentID - 1);
+			} else {
+				this.numSoma += 1;
+			}
 			this.indices.push(this.points.length / 3);
 		} else {
 			this.indices.push(this.points.length / 3);
