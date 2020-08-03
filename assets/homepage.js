29f7ce8c4ad03ee37d21941b70fcdefafd8c3f3b
Will Usher <willusher.life@gmail.com>
2020-08-02 23:01:28 -0600

    update

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,6 +1,10 @@
 window.onload = function() {
     var name = "will";
-    var mail = name + "@" + window.location.hostname;
+    var host = window.location.hostname;
+    if (host.startsWith("www.")) {
+        host = host.substring(4);
+    }
+    var mail = name + "@" + host;
     document.getElementById("envelope").setAttribute("href", "mailto:" + mail);
     document.getElementById("envelope-text").innerHTML = mail;
 }
